261b325fd6e1ae233ca812917c1908cac0fd5a22
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-24 13:11:20 +0700

    Update Prob1_test.py

--- Prob1_test.py
+++ Prob1_test.py
@@ -1,34 +1,20 @@
 import Prob1 as p1
 import numpy as np
 
-'''test x'''
-def test_1():
-    n = 4
-    Aa = [[6,-2,2,4],[12,-8,6,10],[3,-13,9,3],[-6,4,1,-18]]
-    bb = [16,26,-19,-34]
-    A = np.array(Aa,dtype=np.float64)
-    b = np.array(bb, dtype=np.float64)
-    x = p1.Problem1(A,b)
-    assert ( np.absolute(x - np.array([ 3.,  1., -2.,  1.]) )<1e-5).all()
 
-'''test L,U'''
+def test_1():
+    t=(-4,1,2,3,5.0)
+    y = (1182,2,48,272,2262)
+    xtest = list(range(-4,5))
+    ytesting = [p1.Problem1(i,t,y) for i in xtest ]
+    y_correct_ans = [1182.0, 1134.8, 993.2, 757.2, 426.7999999999, 2.0, 48.0, 272.0, 711.5]
+    assert all(list(map(lambda x,y: x-y<1e-8, ytesting,y_correct_ans)))
+
+
 def test_2():
-    n = 4
-    Aa = [[6,-2,2,4],[12,-8,6,10],[3,-13,9,3],[-6,4,1,-18]]
-    A = np.array(Aa,dtype=np.float64)
-    L,U = p1.LUdecomposition(A)
-    Areconstruct = np.dot(L,U)
-    Lans = np.array([[ 1.,   0.,   0.,   0. ], [ 2.,   1.,   0.,   0. ], [ 0.5 , 3.,   1.,   0. ], [-1.,  -0.5,  2.,   1. ]],dtype = np.float64)
-    assert (np.absolute(A-Areconstruct) <1e-8).all() and (np.absolute(L-Lans) <1e-8).all()
-    
-    
-def test_3():
-    n = 4
-    Aa = [[6,-2,2,4],[12,-8,6,10],[3,-13,9,3],[-6,4,1,-18]]
-    bb = [16,26,-19,-34]
-    A = np.array(Aa,dtype=np.float64)
-    b = np.array(bb, dtype=np.float64) 
-    L,U = p1.LUdecomposition(A)
-    z = p1.Lsubstitution(L,b)
-    zans = np.array([16., -6., -9., -3.])
-    assert ( np.absolute(z - zans )<1e-5).all()
+    t=(-1,0,0.5,1,2,5/2)
+    y = (2,1,0,1,2,3)
+    xtest = list(range(-1,2))
+    ytesting = [p1.Problem1(i,t,y) for i in xtest ]
+    y_correct_ans = [757.2, 426.79999999999995, 2.0]
+    assert all(list(map(lambda x,y: x-y<1e-8, ytesting,y_correct_ans)))
