b60d214aa8e7f4d94654ae36fd59c73d649f16a8
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-24 13:30:11 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,7 +1,7 @@
 import numpy as np
 #You may or may not use numpy
 
-def Problem1(x,xi,yi)
+def Problem1(x,xi,yi):
     #Quadratic Spline.
     #Your code here.
     return #Your code here return a scalar
